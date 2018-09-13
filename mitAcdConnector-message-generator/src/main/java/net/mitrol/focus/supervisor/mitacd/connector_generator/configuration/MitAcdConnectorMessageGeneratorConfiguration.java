e13bd22af7c956b9d15b03d9e569c55a8ad89285
Luciano Adassus <ladassus@itcrowdarg.com>
2018-09-13 13:09:04 -0300

    Removed mitAcdConnector-message-generator.
    Signed-off-by: Luciano Adassus <ladassus@itcrowdarg.com>

--- mitAcdConnector-message-generator/src/main/java/net/mitrol/focus/supervisor/mitacd/connector_generator/configuration/MitAcdConnectorMessageGeneratorConfiguration.java
+++
@@ -1,7 +0,0 @@
-package net.mitrol.focus.supervisor.mitacd.connector_generator.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MitAcdConnectorMessageGeneratorConfiguration {
-}
