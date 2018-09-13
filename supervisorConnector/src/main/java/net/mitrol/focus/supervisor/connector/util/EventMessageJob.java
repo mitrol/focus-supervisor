255fa67861aeb0c59d3e01309ac0ce11101d294d
Luciano Adassus <ladassus@itcrowdarg.com>
2018-09-13 12:29:10 -0300

    Little change on event message job.
    Signed-off-by: Luciano Adassus <ladassus@itcrowdarg.com>

--- supervisorConnector/src/main/java/net/mitrol/focus/supervisor/connector/util/EventMessageJob.java
+++ supervisorConnector/src/main/java/net/mitrol/focus/supervisor/connector/util/EventMessageJob.java
@@ -1,17 +1,27 @@
 package net.mitrol.focus.supervisor.connector.util;
 
+import net.mitrol.utils.log.MitrolLogger;
+import net.mitrol.utils.log.MitrolLoggerImpl;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * @author ladassus
  */
 @Component
 public class EventMessageJob implements Job {
+
+    private static MitrolLogger logger = MitrolLoggerImpl.getLogger(EventMessageJob.class);
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         //TODO
+        //JobKey jobKey = context.getJobDetail().getKey();
+        //logger.info("SimpleJob says: " + jobKey + " executing at " + new Date());
     }
 }
