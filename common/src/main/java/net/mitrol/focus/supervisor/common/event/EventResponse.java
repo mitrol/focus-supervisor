40cddd2921d218efa852c1238f53a993d911aa1f
Luciano Adassus <ladassus@itcrowdarg.com>
2018-09-27 15:39:52 -0300

    Common Supervisor Event messages changes.
    Signed-off-by: Luciano Adassus <ladassus@itcrowdarg.com>

--- common/src/main/java/net/mitrol/focus/supervisor/common/event/EventResponse.java
+++ common/src/main/java/net/mitrol/focus/supervisor/common/event/EventResponse.java
@@ -1,82 +1,24 @@
 package net.mitrol.focus.supervisor.common.event;
 
-import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * @author ladassus
  */
 public class EventResponse {
 
-    private String id;
-    private String eventType;
-    private Long refreshInterval;
-    private String dashboardId;
-    private String widgetId;
-    private String widgetType;
-    private String agentId;
-    private List<EventData> data;
+    private EventValues widgetValues;
 
-    public String getId() {
-        return id;
+    public EventValues getWidgetValues() {
+        return widgetValues;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setWidgetValues(EventValues widgetValues) {
+        this.widgetValues = widgetValues;
     }
 
-    public String getEventType() {
-        return eventType;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public Long getRefreshInterval() {
-        return refreshInterval;
-    }
-
-    public void setRefreshInterval(Long refreshInterval) {
-        this.refreshInterval = refreshInterval;
-    }
-
-    public String getDashboardId() {
-        return dashboardId;
-    }
-
-    public void setDashboardId(String dashboardId) {
-        this.dashboardId = dashboardId;
-    }
-
-    public String getWidgetId() {
-        return widgetId;
-    }
-
-    public void setWidgetId(String widgetId) {
-        this.widgetId = widgetId;
-    }
-
-    public String getWidgetType() {
-        return widgetType;
-    }
-
-    public void setWidgetType(String widgetType) {
-        this.widgetType = widgetType;
-    }
-
-    public String getAgentId() {
-        return agentId;
-    }
-
-    public void setAgentId(String agentId) {
-        this.agentId = agentId;
-    }
-
-    public List<EventData> getData() {
-        return data;
-    }
-
-    public void setData(List<EventData> data) {
-        this.data = data;
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
