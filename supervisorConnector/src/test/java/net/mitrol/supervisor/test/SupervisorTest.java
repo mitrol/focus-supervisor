3607428014233fc1b036a11954143f8a5667534d
Luciano Adassus <ladassus@itcrowdarg.com>
2018-09-27 15:40:56 -0300

    Supervisor test update
    Signed-off-by: Luciano Adassus <ladassus@itcrowdarg.com>

--- supervisorConnector/src/test/java/net/mitrol/supervisor/test/SupervisorTest.java
+++ supervisorConnector/src/test/java/net/mitrol/supervisor/test/SupervisorTest.java
@@ -20,17 +20,28 @@ public class SupervisorTest {
     SupervisorEventService eventService;
 
     String eventMessage;
-
+    String eventMessage2;
+    String eventMessage3;
 
     @Before
     public void setUp() {
         eventMessage = "{\"id\": 1,\"eventType\": \"subscribe\",\"refreshInterval\": 2,\"widgetType\": \"INTERACTION_STATES\",\"agentId\": 5,\"filter\": {\"campaignId\": [1, 2, 3],\"groupId\": 1,\"dateTo\": \"21-05-2018 12:00:00\",\"dateFrom\": \"27-05-2018 11:45:30\"}}";
+        eventMessage2 = "{\"id\": 2,\"eventType\": \"subscribe\",\"refreshInterval\": 2,\"widgetType\": \"AGENT_STATES\",\"agentId\": 6,\"filter\": {\"campaignId\": [1, 2, 3],\"groupId\": 1,\"dateTo\": \"21-05-2018 12:00:00\",\"dateFrom\": \"27-05-2018 11:45:30\"}}";
+        eventMessage3 = "{\"id\": 3,\"eventType\": \"unsubscribe\",\"refreshInterval\": 2,\"widgetType\": \"AGENT_STATES\",\"agentId\": 6,\"filter\": {\"campaignId\": [1, 2, 3],\"groupId\": 1,\"dateTo\": \"21-05-2018 12:00:00\",\"dateFrom\": \"27-05-2018 11:45:30\"}}";
     }
 
     @Test
     @Ignore
-    public void test_() {
+    public void test1() throws InterruptedException {
+
         eventService.eventMessageProcess(eventMessage);
-        Assert.assertTrue(true);
+        while (true){
+
+        }
+        //eventService.eventMessageProcess(eventMessage2);
+        //Thread.sleep(20000);
+        //eventService.eventMessageProcess(eventMessage3);
+        //Thread.sleep(20000);
+        //Assert.assertTrue(true);
     }
 }
