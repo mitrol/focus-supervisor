7b8dd7d06686d50fcd23e5cc16720a0a564f223e
Luciano Adassus <ladassus@itcrowdarg.com>
2018-12-16 18:59:07 -0300

    MitAcd client changes.
    Signed-off-by: Luciano Adassus <ladassus@itcrowdarg.com>

--- mitAcdConnector/src/main/java/net/mitrol/focus/supervisor/mitacd/connector/client/SSupMitAcdClientListener.java
+++ mitAcdConnector/src/main/java/net/mitrol/focus/supervisor/mitacd/connector/client/SSupMitAcdClientListener.java
@@ -3,6 +3,4 @@ package net.mitrol.focus.supervisor.mitacd.connector.client;
 import net.mitrol.acd.client.interfaces.MitAcdClientListener;
 
 public interface SSupMitAcdClientListener extends MitAcdClientListener {
-
-    void onReady(SSupMitAcdClient ssupMitAcdClient);
 }
